7dd712aee1dec89c798172e6e8ae094ae2a85bb8
litealex <litealex@gmail.com>
2015-01-08 14:32:51 +0300

    path fix

--- src/js/fm.boot.js
+++ src/js/fm.boot.js
@@ -2,20 +2,20 @@
  * Created by alex on 20.12.14.
  */
 (function (w) {
-    w.fileManager = function (holderElement, editor, cfg) {
+    w.fileManager = function (holderElement, editor, cfg, path) {
         angular.module('fm')
             .factory('editor', function () {
                 return editor;
             })
             .value('fmCfg', {
-                actionsUrl: editor.settings.external_filemanager_path + '/SimpleEditor.ashx',
-                templatesPrefix: editor.settings.external_filemanager_path + '/src/templates/',
+                actionsUrl: path + '/SimpleEditor.ashx',
+                templatesPrefix: path + '/src/templates/',
                 getTemplateUrl: function (template) {
                     return this.templatesPrefix + template + '.html';
                 },
                 imgExtensions: ['jpg', 'gif', 'png', 'jpeg', 'bmp', 'svg'],
-                extensionPrefix512: editor.settings.external_filemanager_path + '/src/img/512px/',
-                extensionPrefix32: editor.settings.external_filemanager_path + '/src/img/32px/',
+                extensionPrefix512: path + '/src/img/512px/',
+                extensionPrefix32: path + '/src/img/32px/',
                 viewType: 'list'
             });
         angular.bootstrap(holderElement, ['fm']);
--- src/js/fm.plugin.js
+++ src/js/fm.plugin.js
@@ -3,15 +3,16 @@
  */
 (function(){
     tinymce.PluginManager.add('fm',function(editor){
+        var path = editor.settings.external_filemanager_path || '';
         editor.addButton('FileManager',{
             text: 'Управление файлами',
             icon: false,
             onclick: function(){
                 $.ajax({
-                    url: editor.settings.external_filemanager_path + '/src/templates/fmModal.html',
+                    url:  path + '/src/templates/fmModal.html',
                     success: function(html){
                         var element = $(html);
-                        fileManager(element[0], editor, {});
+                        fileManager(element[0], editor, {}, path);
                         element.modal();
                     }
                 });
