4c80f4111546bac0e79452980ab3f053f30b3216
litealex <litealex@gmail.com>
2015-01-10 18:21:55 +0300

    add dist

--- gruntfile.js
+++ gruntfile.js
@@ -2,6 +2,36 @@ module.exports = function (grunt) {
     var lessSrc = './src/less/*.less',
         jsSrc = './src/js/*.js';
     grunt.initConfig({
+        copy: {
+            img: {
+                expand: true,
+                cwd: './src/templates/',
+                src: '**',
+                dest: './dist/templates/'
+            },
+            templates: {
+                expand: true,
+                cwd: './src/img/',
+                src: '**',
+                dest: './dist/img/'
+            }
+        },
+        uglify: {
+            dist: {
+                files: {
+                    './dist/js/fm.min.js': [
+                        'app',
+                        'services',
+                        'controllers',
+                        'directives',
+                        'boot',
+                        'plugin'
+                    ].map(function (file) {
+                            return './src/js/fm.' + file + '.js';
+                        })
+                }
+            }
+        },
         jshint: {
             dev: [jsSrc]
         },
@@ -10,12 +40,20 @@ module.exports = function (grunt) {
                 files: {
                     './src/css/fm.css': lessSrc
                 }
+            },
+            dist: {
+                options: {
+                    compress: true
+                },
+                files: {
+                    './dist/css/fm.min.css': lessSrc
+                }
             }
         },
         watch: {
             dev: {
                 files: [lessSrc, jsSrc],
-                tasks: ['less', 'jshint']
+                tasks: ['less:dev', 'jshint']
             }
         }
     });
@@ -24,6 +62,9 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
     grunt.registerTask('default', ['watch']);
+    grunt.registerInitTask('dist', ['uglify', 'less:dist', 'copy']);
 };
--- src/js/fm.plugin.js
+++ src/js/fm.plugin.js
@@ -3,16 +3,16 @@
  */
 (function(){
     tinymce.PluginManager.add('fm',function(editor){
-        var path = editor.settings.external_filemanager_path || '';
+        var options = editor.settings.fm_options;
         editor.addButton('FileManager',{
             text: 'Управление файлами',
             icon: false,
             onclick: function(){
                 $.ajax({
-                    url:  path + '/src/templates/fmModal.html',
+                    url:  options.path + '/templates/fmModal.html',
                     success: function(html){
                         var element = $(html);
-                        fileManager(element[0], editor, {}, path);
+                        fileManager(element[0], editor, {}, options);
                         element.modal();
 
                     }
